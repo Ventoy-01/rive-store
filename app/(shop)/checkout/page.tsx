2d30a0cc1f3fc183397fa18ad26085560821e2c2
Ventoy-01 <jonasclocin01@gmail.com>
2026-07-25 15:10:42 -0400

    delete checkout

--- app/(shop)/checkout/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CheckoutPage() {
-    return (
-        <div className="container mx-auto p-6">
-            <h1 className="text-2xl font-bold">Page de Commande</h1>
-            <p>Paiement en cours de développement...</p>
-        </div>
-    );
-}
